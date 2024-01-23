dc378dd34243fa19cd4281fa2f686dc6b7a8067f
Work <Work@Shahabs-MacBook-Pro.local>
2024-01-23 12:07:54 +0000

    Version one

---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+console.log(`
+Shahab Younas
+younasshahab@gmail.com | +44 7442 410345 | England, United Kingdom
+https://www.linkedin.com/in/shahabyounas/
+https://github.com/shahabyounas
+
+Skills: JavaScript, React, Redux, Python, AWS, GitHub, Next JS, HTML5, CSS3, NextJS, Vue3, Java ,UI/ UX, Ed Tech, Lamda, Serverless
+
+Summary
+● Strong technical problem-solving skills and the ability to identify and resolve existing or potential problems
+● With 5 years of professional experience in software development, I have honed my skills in React, Node, Python, and AWS.
+● Strong hands-on experience with a track record of building complex user interfaces using JavaScript and React JS.
+● Experience in dealing with ambiguity, taking abstract concepts and turning them into working software
+
+Frontend Developer - Huxley ( client Saal.ai ) Abu Dhabi, UAE 2020- Jan 2023
+Saal is an AI company providing end-to-end product development services to large corporations in the Middle East.
+● Developed an AI-based chatbot user interface enabling users to get predictive responses using machine learning models on
+the backend
+● I was involved with frontend design from the ground up, developed a complex EdTech system using ReactJS on the frontend,
+backend was developed using NodeJS, microservices and Kafka for communication among the infrastructure
+● Implemented bi-directional apps and custom localization feature to cater for Arabic applications users
+● Implemented data visualizations dashboards using JavaScript Highchart library and React JS components
+● Optimized performance by applying asynchronous programming and background web worker
+● Build responsive student management system using React JS and Context for state management
+
+Software Engineer - Foretheta (Remote) Seattle, USA 2019- 2020
+Foretheta provides product development, semantic, and AWS cost analysis tools to their potential customers
+● Built a vote tracking system, I was responsible for developing end-to-end solutions. I used React JS for frontend and python
+AWS chalice frame to facilitate the Serverless deployment on AWS Lambda functions
+● Gained broad experience of architecting, designing, and implementing highly available customer-facing applications in
+React, Redux, and using python3 plus AWS cloud environment on the backend
+● Implemented scrapeAPI which would scrap the data from web using python BeautifulSoup and store it in PynamoDB
+
+Software Engineer - VizTeck Solutions and others Islamabad, Pakistan 2017- 2019
+● Developed a scalable cloud-powered backend in NodeJS with the capability of handling multiple operations like payment
+transactions, displaying riders' status on the dashboard and google maps
+● Designed schemas for non-relational and relational databases MongoDB and MySQL Sequelize
+● Integrated third-party payment gateway APIs for European Markets such as Stripe and Braintree
+● Developed responsive web applications and designed REST APIs working closely with the Agile team using JIRA sprints
+● Build Custom WordPress application Integrating WooCommerce and other Popular WordPress Plugins
+● Improved application performance by 30% by introducing cache layers between the client and server persistence
+
+Data Analytics Reports
+
+Analytical Report of Croda International Plc
+Conducted a technical analysis of three entities listed on the London Stock Exchange. Analyzed and manipulated historical price data
+datasets using Python, Pandas, NumPy, and using a Business Intelligence tool to visualize the insights. Applied various statistical
+methods, descriptive statistics and linear regression to find the correlation among the entities.
+
+Pakistan Ecommerce Industry Analysis Report
+Analyzed Pakistan Largest Ecommerce Dataset Available on Kaggle to identify the key trends in the ecommerce market across the
+country. First Performed the Data Processing to clean the Data, remove duplicate, null and redundant data elements using Excel
+Sheet. After analyzing the data, identifying the key metrics, and correlation and finally with help of Business Intelligence -BI tool
+Tableau, visualized the metric for easier understanding.
+
+Masters in FinTech - Huddersfield Business School, University of Huddersfield England, United Kingdom
+Public university West yorkshire Huddersfield, England 2023 - 2024
+Currently enrolled in Fintech Program, completed Managing Big Data and Data Visualization projects with distinction. Gained Hands
+on Experience using BI Tool Tableau, and Python libraries such as pandas, numpy, matplotlib, and seaborn.
+`)
